4603792789fe73779a4c405ba026fcbcefdacaa9
SrDiegoH <diegohenrique4222@gmail.com>
2026-02-16 01:18:58 -0300

    Create utility class

---
+++ utils/utils.py
@@ -0,0 +1,56 @@
+import re
+import requests
+
+def get_substring(text, start_text, end_text, replace_by_paterns=[], should_remove_tags=False):
+    start_index = text.find(start_text)
+    new_text = text[start_index:]
+
+    end_index = new_text[len(start_text):].find(end_text) + len(start_text)
+    cutted_text = new_text[len(start_text):end_index]
+
+    if not cutted_text:
+        return None
+
+    clean_text = cutted_text.replace('\n', '').replace('\t', '')
+
+    no_tags_text = re.sub(r'<[^>]*>', '', clean_text) if should_remove_tags else clean_text
+
+    final_text = no_tags_text
+    for pattern in replace_by_paterns:
+        final_text = final_text.replace(pattern, '')
+
+    return final_text.strip()
+
+def text_to_number(text, should_convert_thousand_decimal_separators=True, convert_percent_to_decimal=False):
+    try:
+        if not text:
+            raise Exception()
+
+        if not isinstance(text, str):
+            return text
+
+        text = text.strip()
+
+        if not text.strip():
+            raise Exception()
+
+        if should_convert_thousand_decimal_separators:
+            text = text.replace('.','').replace(',','.')
+
+        if '%' in text:
+            return float(text.replace('%', '').strip()) / (100 if convert_percent_to_decimal else 1)
+
+        if 'R$' in text:
+            text = text.replace('R$', '')
+
+        return float(text.strip())
+    except:
+        return 0
+
+def request_get(url, headers=None):
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    log_debug(f'Response from {url} : {response}')
+
+    return response
