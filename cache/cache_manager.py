33250f777d83fbd8e62fba941b12a828b2ccc7b2
SrDiegoH <diegohenrique4222@gmail.com>
2026-02-16 01:52:33 -0300

    Refactoring cache manager

--- cache/cache_manager.py
+++ cache/cache_manager.py
@@ -11,11 +11,13 @@ CACHE_FILE_REIT = '/tmp/reit_cache.txt'
 CACHE_FILE_ETF = '/tmp/etf_cache.txt'
 CACHE_FILE_CRIPTO = '/tmp/cripto_cache.txt'
 
-CACHE_EXPIRY = timedelta(days=1)
+_CACHE_EXPIRY = timedelta(days=1)
 
-DATE_FORMAT = '%d-%m-%Y %H:%M:%S'
+_DATE_FORMAT = '%d-%m-%Y %H:%M:%S'
 
-def cache_exists(file):
+_SEPARATOR = '#@#'
+
+def _cache_exists(file):
     if os.path.exists(file):
         return True
 
@@ -26,7 +28,7 @@ def upsert_cache(id, data, file):
     lines = []
     updated = False
 
-    if cache_exists(file):
+    if _cache_exists(file):
         with open(file, 'r') as cache_file:
             lines = cache_file.readlines()
 
@@ -36,24 +38,24 @@ def upsert_cache(id, data, file):
                 cache_file.write(line)
                 continue
 
-            _, old_cached_date_as_text, old_data_as_text = line.strip().split(SEPARATOR)
+            _, old_cached_date_as_text, old_data_as_text = line.strip().split(_SEPARATOR)
             old_data = ast.literal_eval(old_data_as_text)
 
             combined_data = { **old_data, **data }
-            updated_line = f'{id}{SEPARATOR}{old_cached_date_as_text}{SEPARATOR}{combined_data}\n'
+            updated_line = f'{id}{_SEPARATOR}{old_cached_date_as_text}{_SEPARATOR}{combined_data}\n'
             cache_file.write(updated_line)
             updated = True
 
         if not updated:
-            new_line = f'{id}{SEPARATOR}{datetime.now().strftime(DATE_FORMAT)}{SEPARATOR}{data}\n'
+            new_line = f'{id}{_SEPARATOR}{datetime.now().strftime(_DATE_FORMAT)}{_SEPARATOR}{data}\n'
             cache_file.write(new_line)
             log_info(f'New cache entry created for "{id}"')
 
     if updated:
         log_info(f'Cache updated for "{id}"')
 
-def clear_cache(id, file):
-    if not cache_exists(file):
+def _clear_cache(id, file):
+    if not _cache_exists(file):
         return
 
     log_debug('Cleaning cache')
@@ -67,7 +69,7 @@ def clear_cache(id, file):
     log_info(f'Cache cleaning completed for "{id}"')
 
 def read_cache(id, file):
-    if not cache_exists(file):
+    if not _cache_exists(file):
         return None
 
     log_debug('Reading cache')
@@ -79,10 +81,10 @@ def read_cache(id, file):
             if not line.startswith(id):
                 continue
 
-            _, cached_date_as_text, data = line.strip().split(SEPARATOR)
-            cached_date = datetime.strptime(cached_date_as_text, DATE_FORMAT)
+            _, cached_date_as_text, data = line.strip().split(_SEPARATOR)
+            cached_date = datetime.strptime(cached_date_as_text, _DATE_FORMAT)
 
-            if datetime.now() - cached_date <= CACHE_EXPIRY:
+            if datetime.now() - cached_date <= _CACHE_EXPIRY:
                 log_debug(f'Cache hit for "{id}" (Date: {cached_date_as_text})')
                 return ast.literal_eval(data)
 
@@ -91,13 +93,13 @@ def read_cache(id, file):
             break
 
     if clear_cache_control:
-        clear_cache(id, file)
+        _clear_cache(id, file)
 
     log_info(f'No cache entry found for "{id}"')
     return None
 
-def delete_cache(file):
-    if not cache_exists(file):
+def _delete_cache(file):
+    if not _cache_exists(file):
         return
 
     log_debug('Deleting cache')
@@ -108,9 +110,9 @@ def delete_cache(file):
 
 def preprocess_cache(id, file, should_delete_all_cache, should_clear_cached_data, should_use_cache):
     if should_delete_all_cache:
-        delete_cache(file)
+        _delete_cache(file)
     elif should_clear_cached_data:
-        clear_cache(id, file)
+        _clear_cache(id, file)
 
     can_use_cache = should_use_cache and not (should_delete_all_cache or should_clear_cached_data)
 
